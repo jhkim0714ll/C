3e39e7e756e8962c59c056d23d75bf01c2460b4b
김준호 <80739316+rlawnsgh714@users.noreply.github.com>
2021-12-22 13:31:19 +0900

    C언어 수업 격자상의 경로

--- 소스.cpp
+++ 소스.cpp
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#pragma warning(disable : 4996)
+#define size 100
 
-int main() {
-	int a[10] = { 95, 67, 84, 73, 72 , 65, 27, 28, 83, 75 };
-	int per[10] = { 0 };
-	for (int i = 0; i < 10; i++) {
-		per[a[i] / 10]++;
-	}
-	for (int i = 0; i < 10; i++) {
-		printf("%d : %d", i * 10, per[i]);
-		for (int i = 0; i < per[i]; i++) {
-			printf("*");
-		}
-		printf("\n");
-	}
+int map[size][size];
+int map_1[size][size];
+
+int main()
+{
+	int n, m, k;
+	scanf("%d %d %d", &n, &m, &k);
+	return 0;
 }
